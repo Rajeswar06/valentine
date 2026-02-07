c3b85702188c8e3085bcce2d00e0710538b72431
Rajeswar06 <gavidirajeswar@gmail.com>
2026-02-07 19:44:39 +0530

    Move no button on touch

--- script.js
+++ script.js
@@ -4,15 +4,39 @@ const gifResult = document.querySelector(".gif-result");
 const heartLoader = document.querySelector(".cssload-main");
 const yesBtn = document.querySelector(".js-yes-btn");
 const noBtn = document.querySelector(".js-no-btn");
+const noMessages = [
+  "Are you sure?",
+  "Think again!",
+  "Really?",
+  "Please reconsider!",
+  "Don't be so cold!",
+  "Give it another thought!",
+  "Maybe yes?",
+  "One more chance?",
+];
+let lastNoMessageIndex = -1;
 
-// /change the postion of no button
-noBtn.addEventListener("mouseover", () => {
-  const newX = Math.floor(Math.random() * questionContainer.offsetWidth);
-  const newY = Math.floor(Math.random() * questionContainer.offsetWidth);
+const moveNoButton = () => {
+  const maxX = Math.max(0, questionContainer.clientWidth - noBtn.offsetWidth);
+  const maxY = Math.max(0, questionContainer.clientHeight - noBtn.offsetHeight);
+  const newX = Math.floor(Math.random() * maxX);
+  const newY = Math.floor(Math.random() * maxY);
 
   noBtn.style.left = `${newX}px`;
   noBtn.style.top = `${newY}px`;
-});
+
+  let nextIndex = Math.floor(Math.random() * noMessages.length);
+  if (nextIndex === lastNoMessageIndex) {
+    nextIndex = (nextIndex + 1) % noMessages.length;
+  }
+  lastNoMessageIndex = nextIndex;
+  noBtn.textContent = noMessages[nextIndex];
+};
+
+// /change the postion of no button
+noBtn.addEventListener("mouseover", moveNoButton);
+noBtn.addEventListener("pointerdown", moveNoButton);
+noBtn.addEventListener("touchstart", moveNoButton, { passive: true });
 
 // yes button functionality
 
